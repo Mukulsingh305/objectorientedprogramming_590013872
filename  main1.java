2b0b96a4d3f000efda521f4eb62e43617849afb3
objectorientedprogramming_590013872 <mukulsingh120306@gmail.com>
2026-01-29 12:38:45 +0530

    Add files via upload

---
+++  main1.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+
+class main1 {
+    int n;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        main1 m = new main1();
+
+        m.n = s.nextInt();    
+        System.out.println(m.n);   
+
+        s.close();
+    }
+}
