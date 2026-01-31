ca3f3d6613495e6f60725ae66057c5ea1d8d9825
objectorientedprogramming_590013872 <mukulsingh120306@gmail.com>
2026-02-01 00:38:38 +0530

    Add files via upload

---
+++ commandline.java
@@ -0,0 +1,9 @@
+ class commandline {
+    public static void main(String[] args) {
+
+        System.out.println("Array elements:");
+        for(String s : args){
+            System.out.print(s + " ");
+        }
+    }
+}
---
+++ sumofarraydeclaringinside.java
@@ -0,0 +1,21 @@
+ import java.util.Scanner;
+
+public class sumofarraydeclaringinside {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int arr[] = new int[5];
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("enter 5 numbers");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = s.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.println("sum " + sum);
+    }
+}
